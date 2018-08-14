d3766425c186791a6c6336bd0d27bdceded802bc
Jorge Fernandez <jorge@wodanbrothers.com>
2018-08-15 00:41:53 +0200

    Add previous card style

--- lib/ui/lessons.dart
+++ lib/ui/lessons.dart
@@ -3,27 +3,13 @@ import 'package:flutter/material.dart';
 class Lessons extends StatefulWidget {
 
   @override
-  State<StatefulWidget> createState() {
-    return new _LessonsState();
-  }
-
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: AppBar(title: Text('Afspraken')),
-//      body: new Container(
-//        alignment: Alignment.topLeft,
-//        child: Text('Een overzicht van alle lessen'),
-//      ),
-//    );
-
-//  }
+  State<StatefulWidget> createState() => new _LessonsState();
 }
 
 
 class _LessonsState extends State<Lessons> {
 
-  final List _lessons = List.generate(1000, (i) => "Item number $i");
+  final List _lessons = List.generate(30, (i) => "Item number $i");
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +23,43 @@ class _LessonsState extends State<Lessons> {
     return new ListView.builder(
       itemCount: _lessons.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(_lessons[index]),
-        );
+        return _buildAppointmentItem(index);
       }
     );
+  }
+
+  Widget _buildAppointmentItem(int index) {
+    return new Padding(
+      padding: new EdgeInsets.all(5.0),
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+                leading: Icon(Icons.access_time, color: Colors.blue),
+                title: Text(_lessons[index])
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border( top: BorderSide(color: Colors.blue[200]))),
+              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+              child: Row(
+                children: <Widget>[
+
+                  Icon(Icons.timer),
+                  Container(width: 10.0),
+                  Text('19:00'),
+                  Container(width: 15.0),
+                  Text('--:--'),
+
+                  Expanded(
+                    child: Text('70 min', textAlign: TextAlign.right),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ),
+    );//Card(child: Text(_lessons[index]));
   }
 
 
